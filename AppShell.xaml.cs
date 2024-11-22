f149eafac7c11ec28954e9c86fb3157a22a584f7
antoneni2000 <neniscaaa@yahoo.com>
2024-11-22 17:02:49 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NeniscaAntoniaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NeniscaAntoniaLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
